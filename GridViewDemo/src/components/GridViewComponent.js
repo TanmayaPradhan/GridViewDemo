104197007f4f9aba593b96e1c595027d40fd9a11
tanmaya <pradhan.tanmaya8@gmail.com>
2020-02-15 00:24:23 +0530

    ui

--- GridViewDemo/src/components/GridViewComponent.js
+++ GridViewDemo/src/components/GridViewComponent.js
@@ -1,79 +1,88 @@
+/**
+ * AUTHOR: TANMAYA PRADHAN
+ * PURPOSE : DESIGNING THE GRID VIEW WITH EQUAL MARGIN GAP
+ * DATE: 14/02/2020
+ */
+
+/**
+ * TO TEST THE GRID VIEW, CHANGE THE VALUE OF "itemListLength" IN constructor.
+ * IF YOU ASSIGN 6 IT WILL SHOW 3*3 VIEWS, IF YOU ASSIGN 8 IT WILL SHOW 4*4 ACCORDINGLY AND SO ON.
+ * I HAVE ASSIGNED "margin" IN A GLOBAL VARIABLE.YOU CAN CHANGE THAT VALUE, THEN ALL THE MARGINS WILL BE ADJUSTED ACCORDINGLY.
+ * HERE I HAVE CHANGED THE HEIGHT OF THE VIEW DYNAMICALLY ALSO. IF YOU DONT NEED THAT CAN ASSIGN A FIX VALUE IN "GridViewCell".
+ */
+
 import React, { Component } from 'react'
-import {SafeAreaView,View,StyleSheet,FlatList} from 'react-native';
+import { SafeAreaView, View, StyleSheet, FlatList } from 'react-native';
 import GridViewCell from './subviews/GridViewCell';
 
 var numOfRows = 2
 var numOfColumns = 0
 var margin = 10
 class componentName extends Component {
-    constructor (props) {
-        super(props)
-        this.state={
-            cellWidth: 0,
-            cellHeight:0,
-            itemListLength:11,
-            itemList:[],
-        }
-        this.fatListRef = null;
+  constructor(props) {
+    super(props)
+    this.state = {
+      cellWidth: 0,
+      cellHeight: 0,
+      itemListLength: 10,
+      itemList: [],
     }
-    
-    componentDidMount () {
-        let arr = []
-        for(let i=0; i< this.state.itemListLength;i++){
-            arr.push(i)
-        }
-        this.setState({
-            itemList: arr
-        })
-    }
-    find_dimesions(layout){
-        const {x, y, width, height} = layout;
-        if(this.state.itemListLength % 2 == 0){
-            numOfColumns = this.state.itemListLength/2;
-        }
-        else{
-            numOfColumns = (this.state.itemListLength - 1)/2;
-        }
-        
-        let verticalMarginTobeDeducted = (numOfRows + 1) * margin
-        let horizontalMarginTobeDeducted = (numOfColumns + 1) * margin;
-        this.setState({
-            cellWidth: (width - horizontalMarginTobeDeducted) / numOfColumns,
-            cellHeight: (height-  verticalMarginTobeDeducted) / numOfRows,
-        })
-      }
-  render () {
-      
-    return (
-        <SafeAreaView style={styles.SAFEAREA}>
 
-        
-      <View style={styles.CONTAINER}
-      onLayout={(event) => { this.find_dimesions(event.nativeEvent.layout) }} 
-      >
-      {
-          numOfColumns == 0 ? null :
-      
-          <FlatList
-          ref={(ref)=> this.fatListRef = ref}
-          data={this.state.itemList}
-          style={{marginLeft: margin,marginTop: margin}}
-          numColumns={numOfColumns}
-          renderItem={({ item }) => <GridViewCell cellWidth={this.state.cellWidth} margin={margin} cellHeight={this.state.cellHeight} />}
-          keyExtractor={(item, index) => index.toString()}
-          //getItmeLayout={this.getItemLayout}
-          ></FlatList>
-        }
-      </View>
+  }
+
+  componentDidMount() {
+    let arr = []
+    for (let i = 0; i < this.state.itemListLength; i++) {
+      arr.push(i)
+    }
+    this.setState({
+      itemList: arr
+    })
+  }
+  find_dimesions(layout) {
+    const { x, y, width, height } = layout;
+    if (this.state.itemListLength % 2 == 0) {
+      numOfColumns = this.state.itemListLength / 2;
+    }
+    else {
+      numOfColumns = (this.state.itemListLength - 1) / 2;
+    }
+
+    let verticalMarginTobeDeducted = (numOfRows + 1) * margin
+    let horizontalMarginTobeDeducted = (numOfColumns + 1) * margin;
+    this.setState({
+      cellWidth: (width - horizontalMarginTobeDeducted) / numOfColumns,
+      cellHeight: (height - verticalMarginTobeDeducted) / numOfRows,
+    })
+  }
+  render() {
+
+    return (
+      <SafeAreaView style={styles.SAFEAREA}>
+
+
+        <View style={styles.CONTAINER}
+          onLayout={(event) => { this.find_dimesions(event.nativeEvent.layout) }}
+        >
+          {
+            numOfColumns == 0 ? null :
+
+              <FlatList
+                data={this.state.itemList}
+                style={{ marginLeft: margin, marginTop: margin }}
+                numColumns={numOfColumns}
+                renderItem={({ item }) => <GridViewCell cellWidth={this.state.cellWidth} margin={margin} cellHeight={this.state.cellHeight} />}
+                keyExtractor={(item, index) => index.toString()}
+              ></FlatList>
+          }
+        </View>
       </SafeAreaView>
     )
   }
-  getItemLayout = (data, index) => (
-    {length: 40, offset: 40 * index, index}
-);
+
 }
 const styles = StyleSheet.create({
-  SAFEAREA:{flex:1,backgroundColor:"#ccc",},
-  CONTAINER: {flex:1,backgroundColor:"#090",},
+  SAFEAREA: { flex: 1, backgroundColor: "#fff", },
+  CONTAINER: { flex: 1, backgroundColor: "transparent", },
 })
 export default componentName
--- GridViewDemo/src/components/subviews/GridViewCell.js
+++ GridViewDemo/src/components/subviews/GridViewCell.js
@@ -1,13 +1,13 @@
 import React, { PureComponent } from 'react'
-import {View,StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 class GridViewCell extends PureComponent {
-  render () {
+  render() {
     return (
-      <View style={[styles.CONTAINER,{width: this.props.cellWidth,height: this.props.cellHeight,marginRight:this.props.margin,marginBottom: this.props.margin}]}></View>
+      <View style={[styles.CONTAINER, { width: this.props.cellWidth, height: this.props.cellHeight, marginRight: this.props.margin, marginBottom: this.props.margin }]}></View>
     )
   }
 }
 const styles = StyleSheet.create({
-  CONTAINER:{height:0,width:0,backgroundColor:"red",justifyContent:"center"}
+  CONTAINER: { height: 0, width: 0, backgroundColor: "#ddd", justifyContent: "center" }
 })
 export default GridViewCell
